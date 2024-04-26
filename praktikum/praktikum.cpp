6ae36198d3769a8d9362966d404d73aa158c6646
FauzanRizkiAfri <163797425+FauzanRizkiAfri@users.noreply.github.com>
2024-04-26 09:31:43 +0700

    1

--- praktikum/praktikum.cpp
+++ praktikum/praktikum.cpp
@@ -5,4 +5,15 @@ using namespace std;
 int main()
 {
     std::cout << "Hello World!\n";
-}
+#include <iostream>
+	using namespace std;
+
+	struct Node {
+		int noMhs;
+		string name;
+		Node* next;
+	};
+
+	Node* START = NULL;
+
+
